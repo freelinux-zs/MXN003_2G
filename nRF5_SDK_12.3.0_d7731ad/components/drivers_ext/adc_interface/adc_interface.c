67d9cef15ea6263572aaba8df131fb735ae840ef
freelinux-zs <zhoubin.xu@foxmail.com>
2017-06-17 17:12:47 +0800

    ADC设置

--- nRF5_SDK_12.3.0_d7731ad/components/drivers_ext/adc_interface/adc_interface.c
+++ nRF5_SDK_12.3.0_d7731ad/components/drivers_ext/adc_interface/adc_interface.c
@@ -1,8 +1,6 @@
 #include "nrf_drv_adc.h"
 #include "app_error.h"
 
-
-static nrf_drv_adc_channel_t m_channel_config = NRF_DRV_ADC_DEFAULT_CHANNEL(NRF_ADC_CONFIG_INPUT_7); /**< Channel instance. Default configuration used. */
 static nrf_adc_value_t adc_buf[1];
 
 #define ADC_REF_VBG_VOLTAGE_IN_MILLIVOLTS 1200   
@@ -29,7 +27,7 @@ static void adc_event_handler(nrf_drv_adc_evt_t const * p_event)
 			
 				batt_lvl_in_milli_volts = ADC_RESULT_IN_MILLI_VOLTS(adc_result);// +
                                  // DIODE_FWD_VOLT_DROP_MILLIVOLTS;
-				printf("v:%d\r\n",batt_lvl_in_milli_volts);
+				printf("AT+BATVAL:%d\r\n",batt_lvl_in_milli_volts);
 				
     }
 }
@@ -38,15 +36,16 @@ static void adc_event_handler(nrf_drv_adc_evt_t const * p_event)
 
 void adc_config_init(void)
 {
-    ret_code_t ret_code;
-    nrf_drv_adc_config_t config = NRF_DRV_ADC_DEFAULT_CONFIG;
+    ret_code_t err_code = nrf_drv_adc_init(NULL, adc_event_handler);
+    APP_ERROR_CHECK(err_code);
 
-    ret_code = nrf_drv_adc_init(&config, adc_event_handler);
-    APP_ERROR_CHECK(ret_code);
+    static nrf_drv_adc_channel_t channel =
+        NRF_DRV_ADC_DEFAULT_CHANNEL(NRF_ADC_CONFIG_INPUT_7);
+    // channel.config.config.input = NRF_ADC_CONFIG_SCALING_SUPPLY_ONE_THIRD;
+    channel.config.config.input = (uint32_t)NRF_ADC_CONFIG_SCALING_SUPPLY_ONE_THIRD;
+    nrf_drv_adc_channel_enable(&channel);
 
-    nrf_drv_adc_channel_enable(&m_channel_config);
-	
-		nrf_drv_adc_buffer_convert(&adc_buf[0], 1);
+    err_code = nrf_drv_adc_buffer_convert(&adc_buf[0], 1);
 }
 
 
--- nRF5_SDK_12.3.0_d7731ad/components/drivers_ext/adc_interface/adc_interface.h
+++ nRF5_SDK_12.3.0_d7731ad/components/drivers_ext/adc_interface/adc_interface.h
@@ -2,6 +2,6 @@
 #define __ADC_INTERFACE_H
 /*≥ı ºªØADC*/
 extern void adc_config_init(void);
-
+extern void start_read_adc(void);
 #endif
 
