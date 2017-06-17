f848eb4ef9fe57c6bd972f0ecdc9b01840c10721
freelinux-zs <zhoubin.xu@foxmail.com>
2017-06-17 17:13:18 +0800

    2G 模块控制接口

--- nRF5_SDK_12.3.0_d7731ad/components/drivers_ext/modem_2g/modem_2g.h
+++ nRF5_SDK_12.3.0_d7731ad/components/drivers_ext/modem_2g/modem_2g.h
@@ -9,19 +9,22 @@
 #include "nrf_gpio.h"
 #include "mxn003.h"
 
-__STATIC_INLINE void modem_2g_open(uint32_t pin_number)
+#define  MODME_CONTRL_PIN MODEM_2G_PIN_NUMBER
+
+__STATIC_INLINE void modem_2g_open(void)
 {
-	nrf_gpio_pin_write(pin_number, 0);
+	nrf_gpio_pin_write(MODME_CONTRL_PIN, 0);
 }
 
-__STATIC_INLINE void modem_2g_close(uint32_t pin_number)
+__STATIC_INLINE void modem_2g_close(void)
 {
-	nrf_gpio_pin_write(pin_number, 1);
+	nrf_gpio_pin_write(MODME_CONTRL_PIN, 1);
 }
 
-__STATIC_INLINE void modem_2g_init(uint32_t pin_number)
+__STATIC_INLINE void modem_2g_init(void)
 {
-	nrf_gpio_cfg_output(pin_number);
+	nrf_gpio_cfg_output(MODME_CONTRL_PIN);
+	nrf_gpio_pin_write(MODME_CONTRL_PIN, 1);
 }
 
 #endif
